ee3bf8b2314c1de52eebca0e6367ffba199a6801
IStayzy <44355559+IStayzy@users.noreply.github.com>
2020-11-04 21:28:14 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Xml.Serialization;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+#region     Some
+        static void Main(string[] args)
+        {
+            string age = Console.ReadLine();
+
+            Console.WriteLine();
+
+            int ageInt;
+
+            bool some = int.TryParse(age, out ageInt);
+
+            if (some == true)
+                Console.WriteLine(age);
+
+            else
+                Console.WriteLine("Sorry, you not correct age");
+
+            Console.ReadLine();
+        }
+        #endregion
+    }
+}
